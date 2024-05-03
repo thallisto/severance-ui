5c461c140836cc259aec95c9cff4ace36c5ef6cf
Leonardo Gruppelli <leonardo.gruppelli@gmail.com>
2024-05-02 22:00:37 -0300

    build: update vite config

--- vite.config.ts
+++ vite.config.ts
@@ -1,22 +1,28 @@
-import { defineConfig } from 'vite'
-import { resolve } from "path";
-import Vue from '@vitejs/plugin-vue'
+import { defineConfig } from 'vite';
+import { resolve } from 'path';
+import Vue from '@vitejs/plugin-vue';
+import AutoImport from 'unplugin-auto-import/vite';
 
 export default defineConfig({
   build: {
     lib: {
-      entry: resolve(__dirname, "src/index.ts"),
-      name: "Severance UI",
-      fileName: "severance-ui",
+      entry: resolve(__dirname, 'src/index.ts'),
+      name: 'Severance UI',
+      fileName: 'severance-ui',
     },
     rollupOptions: {
-      external: ["vue"],
+      external: ['vue'],
       output: {
         globals: {
-          vue: "Vue",
+          vue: 'Vue',
         },
       },
     },
   },
-  plugins: [Vue()],
-})
+  plugins: [
+    Vue(),
+    AutoImport({
+      imports: ['vue'],
+    }),
+  ],
+});
